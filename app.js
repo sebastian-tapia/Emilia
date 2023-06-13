7ececa8c1fae0d40f6a4ff0ee855121af180de84
sebastian-tapia <sebastian.tapia35202@gmail.com>
2023-06-13 12:41:43 -0300

    estructura inicial

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3030;
+const path= require('path');
+
+
+app.get('/', (req,res)=>res.sendFile(path.join(__dirname,'views','home.html')))
+app.get('/about',(req,res)=>res.sendFile(path.join(__dirname,'views','about.html')))
+
+app.get('/contact', (req,res)=>res.sendFile(path.join(__dirname,'views','contact.html')))
+
+app.get('/music',(req,res)=>res.sendFile(path.join(__dirname,'views','music.html')))
+
+app.get('/*',(req,res)=>res.sendFile(path.join(__dirname,'views','404.html')) )
+
+app.listen(PORT, ()=> console.log(`Servidor corriendo en http://localhost:${PORT}`))
